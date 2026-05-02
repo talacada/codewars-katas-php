0a733f474885c0b619ee5455044db7e7d81d28d4
talacada <atalac@proton.me>
2026-05-02 22:45:30 +0200

    ZonkGame finished

---
+++ Tests/Y2026/Q2/ScreenLockingPatternsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Y2026\Q2;
+
+use Kata\Y2026\Q2\ScreenLockingPatterns;
+use PHPUnit\Framework\TestCase;
+
+function countPatternsFrom(string $f, int $l) : int {
+	$lock = new ScreenLockingPatterns($f, $l);
+	return $lock->getNumberOfCombinations();
+}
+class ScreenLockingPatternsTest extends TestCase {
+	public function testBasicTests() {
+		$this->assertSame(0, countPatternsFrom('A', 0));
+		$this->assertSame(0, countPatternsFrom('A', 10));
+		$this->assertSame(1, countPatternsFrom('B', 1));
+		$this->assertSame(5, countPatternsFrom('C', 2));
+		$this->assertSame(37, countPatternsFrom('D', 3));
+		$this->assertSame(256, countPatternsFrom('E', 4));
+		$this->assertSame(23280, countPatternsFrom('E', 8));
+	}
+}
