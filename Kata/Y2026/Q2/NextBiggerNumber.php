e6a1606fd11d3ecf8ae05da62621f90fa237ddd5
talacada <atalac@proton.me>
2026-05-10 15:43:17 +0200

    NextBiggerNumber day 1

---
+++ Kata/Y2026/Q2/NextBiggerNumber.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Kata\Y2026\Q2;
+
+use PHPUnit\Framework\TestCase;
+
+/*
+
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+  12 ==> 21
+ 513 ==> 531
+2017 ==> 2071
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift, None in Rust):
+
+  9 ==> -1
+111 ==> -1
+531 ==> -1
+
+
+*/
+
+function nextBigger(int $n):int
+{
+	$digits = str_split( (string)($n));
+	if (count(array_unique($digits)) === 1) {
+		return -1;
+	}
+
+	for ($i = count($digits) - 1; $i > -1; $i--) {
+		for ($j = count($digits) - 2; $j > -1; $j--) {
+			if ($digits[$i] > $digits[$j]) {
+				$biggerNumber = $digits;
+				$biggerNumber[$j] =  $digits[$i];
+				$biggerNumber[$i] = $digits[$j];
+				//TODO FIX HERE - nedokazu splitnout to co neorderovat a to co ma jit na order. Nakonec se jenom spoji OK část a seřazená část
+				$needOrdering = $biggerNumber;
+				array_splice($needOrdering, $j);
+				array_splice($biggerNumber, 0, $j);
+				sort($needOrdering);
+				$final = array_merge($biggerNumber, $needOrdering);
+				return $final;
+			}
+		}
+	}
+
+	/*
+	$possibleNumbers = gmp_fact(intval($digits));
+	$lastIndex = array_key_last($digits);
+	$subLastIndex = array_key_last($digits) - 1;
+
+
+	for ($i = 1; $i < $possibleNumbers; $i++) {
+		$n1 = $digits[$lastIndex];
+		$n2 = $digits[$subLastIndex];
+		$newDigits = $digits;
+
+		$newDigits[$lastIndex] = $n1;
+		$newDigits[$subLastIndex] = $n2;
+
+		$newDigits = intval(implode('', $newDigits));
+
+		if ($newDigits > $n) {
+			return $newDigits;
+		}
+
+
+	}*/
+}
+
+class NextBiggerNumber extends TestCase
+{
+	public function testBasicTests()
+	{
+		//$this->assertSame(21, nextBigger(12));
+		$this->assertSame(531, nextBigger(513));
+		$this->assertSame(2071, nextBigger(2017));
+		$this->assertSame(441, nextBigger(414));
+		$this->assertSame(414, nextBigger(144));
+		$this->assertSame(123456798, nextBigger(123456789));
+		$this->assertSame(1234567908, nextBigger(1234567890));
+		$this->assertSame(-1, nextBigger(9876543210));
+		$this->assertSame(-1, nextBigger(9999999999));
+		$this->assertSame(59884848483559, nextBigger(59884848459853));
+	}
+}
+
