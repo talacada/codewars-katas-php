2f4f5348f8c4393f9eddba47615e671de271a0f9
talacada <atalac@proton.me>
2026-05-12 11:37:32 +0200

    NextBiggerNumber day 3 FINISHED

--- Kata/Y2026/Q2/NextBiggerNumber.php
+++ Kata/Y2026/Q2/NextBiggerNumber.php
@@ -23,26 +23,22 @@ If the digits can't be rearranged to form a bigger number, return -1 (or nil in 
 function nextBigger(int $n):int
 {
 	$digits = str_split( (string)($n));
-	if (count(array_unique($digits)) === 1) {
-		return -1;
-	}
+	$digitsCount = count($digits);
 
-	//TODO vybiram úplně špatná čísla, viz obrázek. Musím vybrat to co má nejnižší $j.
-	for ($i = count($digits) - 1; $i > -1; $i--) {
-		for ($j = $i - 1; $j > -1; $j--) {
-			if ($digits[$i] > $digits[$j]) {
+	for ($pivotIndex = $digitsCount - 2; $pivotIndex > -1; $pivotIndex--) {
+		for ($swapIndex = $digitsCount - 1; $swapIndex > $pivotIndex; $swapIndex--) {
+			if ($digits[$swapIndex] > $digits[$pivotIndex]) {
 				$biggerNumber = $digits;
-				$biggerNumber[$j] =  $digits[$i];
-				$biggerNumber[$i] = $digits[$j];
-				$left = array_slice($biggerNumber, 0, $j + 1);
-				$right = array_slice($biggerNumber, $j + 1);
+				$biggerNumber[$pivotIndex] =  $digits[$swapIndex];
+				$biggerNumber[$swapIndex] = $digits[$pivotIndex];
+				$left = array_slice($biggerNumber, 0, $pivotIndex + 1);
+				$right = array_slice($biggerNumber, $pivotIndex + 1);
 				sort($right);
 				$final = array_merge($left, $right);
 				return (int) implode('', $final);
 			}
 		}
 	}
-
 	return -1;
 }
 
@@ -50,15 +46,21 @@ class NextBiggerNumber extends TestCase
 {
 	public function testBasicTests()
 	{
-		//$this->assertSame(21, nextBigger(12));
-		//$this->assertSame(531, nextBigger(513));
-		//$this->assertSame(2071, nextBigger(2017));
-		//$this->assertSame(441, nextBigger(414));
-		//$this->assertSame(414, nextBigger(144));
-		//$this->assertSame(123456798, nextBigger(123456789));
-		//$this->assertSame(1234567908, nextBigger(1234567890));
-		//$this->assertSame(-1, nextBigger(9876543210));
-		//$this->assertSame(-1, nextBigger(9999999999));
+		$this->assertSame(21, nextBigger(12));
+		$this->assertSame(531, nextBigger(513));
+		$this->assertSame(2071, nextBigger(2017));
+		$this->assertSame(441, nextBigger(414));
+		$this->assertSame(414, nextBigger(144));
+		$this->assertSame(151, nextBigger(115));
+		$this->assertSame(-1, nextBigger(550));
+		$this->assertSame(1200, nextBigger(1020));
+		$this->assertSame(12223233, nextBigger(12222333));
+		$this->assertSame(-1, nextBigger(33322211));
+		$this->assertSame(-1, nextBigger(100));
+		$this->assertSame(123456798, nextBigger(123456789));
+		$this->assertSame(1234567908, nextBigger(1234567890));
+		$this->assertSame(-1, nextBigger(9876543210));
+		$this->assertSame(-1, nextBigger(9999999999));
 		$this->assertSame(59884848483559, nextBigger(59884848459853));
 	}
 }
