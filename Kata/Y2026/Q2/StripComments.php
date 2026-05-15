3197d5488a3c47c0bc9eb8972bf92d65003dfd9a
talacada <atalac@proton.me>
2026-05-15 18:40:06 +0200

    test

--- Kata/Y2026/Q2/StripComments.php
+++ Kata/Y2026/Q2/StripComments.php
@@ -30,7 +30,7 @@ function stripComments(string $str, array $markers): string
 
 	foreach ($explodedString as $line) {
 		$found = false;
-		$markerIndex = [];
+		$markerIndex = [];//test
 
 		foreach ($markers as $marker) {
 			$index = strpos($line, $marker);
