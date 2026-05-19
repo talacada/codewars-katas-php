6a2f07b421e5509777f58e450c09e6ffbcd69fa1
talacada <atalac@proton.me>
2026-05-19 14:37:19 +0200

    SumOfIntervals day 1

---
+++ Kata/Y2026/Q2/SumOfIntervals.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+Intervals
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+Overlapping Intervals
+List containing overlapping intervals:
+
+[
+   [1, 4],
+   [7, 10],
+   [3, 5]
+]
+The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+Examples:
+sumIntervals( [
+   [1, 2],
+   [6, 10],
+   [11, 15]
+] ) => 9
+
+sumIntervals( [
+   [1, 4],
+   [7, 10],
+   [3, 5]
+] ) => 7
+
+sumIntervals( [
+   [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ) => 19
+
+sumIntervals( [
+   [0, 20],
+   [-100000000, 10],
+   [30, 40]
+] ) => 100000030
+Tests with large intervals
+Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
+
+https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/php
+*/
+
+namespace Kata\Y2026\Q2;
+
+use PHPUnit\Framework\TestCase;
+
+function sum_intervals(array $intervals): int {
+	$sum = 0;
+	$counted = [];
+	foreach ($intervals as $interval) {
+		foreach ($counted as $singleCount) {
+			//Bigger interval eated whole smaller interval
+			if ($interval[0] >= $singleCount[0] && $interval[1] <= $singleCount[1]) {
+				$interval = [];
+				break;
+			//Only the end of interval was eaten
+			}elseif ($interval[0] < $singleCount[0] && ($interval[1] >= $singleCount[0] && $interval[1] <= $singleCount[0])) {
+				$interval = [$interval[0], $singleCount[0]];
+			//Only the start of interval was eaten
+			}elseif ($interval[0] < $singleCount[0] && ($interval[1] >= $singleCount[0] && $interval[1] <= $singleCount[0])) {//TODO
+				$counted = 'dd';
+			//Only a portion in middle was ean by smaller interval
+			}elseif ($interval[0] < $singleCount[0] && ($interval[1] >= $singleCount[0] && $interval[1] <= $singleCount[0])) {//TODO
+				$counted = 'dd';
+			}
+		}
+	}
+	return $sum;
+}
+
+class SumOfIntervals extends TestCase {
+	public function testExamples() {
+		// Non-overlapping intervals
+		$this->assertSame(4, sum_intervals([[1, 5]]));
+		$this->assertSame(8, sum_intervals([
+			[1, 5],
+			[6, 10]
+		]));
+		// Overlapping intervals
+		$this->assertSame(4, sum_intervals([
+			[1, 5],
+			[1, 5]
+		]));
+		$this->assertSame(7, sum_intervals([
+			[1, 4],
+			[7, 10],
+			[3, 5]
+		]));
+	}
+
+	public function testLargeIntervals() {
+		$this->assertSame((int)2e9, sum_intervals([[(int)-1e9, (int)1e9]]));
+		$this->assertSame((int)1e8 + 30, sum_intervals([
+			[0, 20],
+			[(int)-1e8, 10],
+			[30, 40]
+		]));
+	}
+}
