c92a4ce4649b2c50e9944ad897b195079c9e19cb
talacada <atalac@proton.me>
2026-05-28 20:55:50 +0200

    TheObservedPin Prep

---
+++ Tests/Y2026/Q2/TheObservedPinTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Y2026\Q2;
+
+use Kata\Y2026\Q2\TheObservedPin;
+use PHPUnit\Framework\TestCase;
+
+function getPINs($pin) {
+	$TheObservedPin = new TheObservedPin($pin);
+	return $TheObservedPin->solve();
+}
+
+class TheObservedPinTest extends TestCase
+{
+	// test function names should start with "test"
+	public function testSample() {
+		$expectations = [
+			"8" => ["5", "7", "8", "9", "0"],
+			"11" => ["11", "22", "44", "12", "21", "14", "41", "24", "42"],
+			"369" => ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"],
+		];
+		foreach ($expectations as $pin => $expect) {
+			$actual = getPINs($pin);
+			sort($actual);
+			sort($expect);
+			$this->assertSame($expect, $actual);
+		}
+	}
+}
