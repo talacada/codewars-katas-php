6160fc9fa9084f4619d1afbd4841367b1ad2f0b8
talacada <atalac@proton.me>
2026-06-02 15:47:33 +0200

    SmallestPossibleSum day 2

--- Kata/Y2026/Q2/SmallestPossibleSum.php
+++ Kata/Y2026/Q2/SmallestPossibleSum.php
@@ -38,12 +38,52 @@ function solution($input):int
 	return array_sum($input);
 }
 */
-
+/*
+function solution($input):int
+{
+	rsort($input);
+	do {
+		$changed = false;
+		$unique = array_values(array_unique($input));
+		if (isset($unique[1]) && $unique[0] > $unique[1]) {
+			if ($unique[0] % $unique[1] != 0) {
+				$input[0] = $unique[0] % $unique[1];
+			}else {
+				$input[0] = $unique[0] - $unique[1];
+			}
+			$changed = true;
+			rsort($input);
+		}
+	}while ($changed);
+	return array_sum($input);
+}
+*/
+//TODO udelat to pres foreach a ten modulus v nem pro vsechny mezi vsemi.
+function solution($input):int
+{
+	rsort($input);
+	do {
+		$changed = false;
+		$unique = array_values(array_unique($input));
+		if (isset($unique[1]) && $unique[0] > $unique[1]) {
+			if (($unique[0] % $unique[1]) === 0) {
+				$input[0] = $unique[1];
+			}else {
+				$input[0] = $unique[0] % $unique[1];
+			}
+			$changed = true;
+			rsort($input);
+		}
+	}while ($changed);
+	return array_sum($input);
+}
 class SmallestPossibleSum extends TestCase
 {
 	public function testExample()
 	{
 		$this->assertSame(9, solution([6, 9, 21]));
+		$this->assertSame(18, solution([18, 3, 3, 3, 3, 3]));
+		$this->assertSame(2, solution([10000000000, 1]));
 		$this->assertSame(3, solution([1, 21, 55]));
 		$this->assertSame(5, solution([3, 13, 23, 7, 83]));
 		$this->assertSame(12, solution([4, 16, 24]));
