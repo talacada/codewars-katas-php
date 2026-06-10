511a4e8d09bcbc0dc526de2e138998503663df65
talacada <atalac@proton.me>
2026-06-10 12:27:14 +0200

    RoboScript1

---
+++ Kata/Y2026/Q2/RoboScript1.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+RoboScript #1 - Implement Syntax Highlighting
+Disclaimer
+The story presented in this Kata Series is purely fictional; any resemblance to actual programming languages, products, organisations or people should be treated as purely coincidental.
+
+About this Kata Series
+This Kata Series is based on a fictional story about a computer scientist and engineer who owns a firm that sells a toy robot called MyRobot which can interpret its own (esoteric) programming language called RoboScript. Naturally, this Kata Series deals with the software side of things (I'm afraid Codewars cannot test your ability to build a physical robot!).
+
+Story
+You are a computer scientist and engineer who has recently founded a firm which sells a toy product called MyRobot which can move by receiving a set of instructions by reading a file containing a script. Initially you have planned the robot to be able to interpret JavaScript files for its movement instructions but you later decided that it would make MyRobot too hard to operate for most customers out there who aren't even computer programmers in the first place. For this reason, you have decided to invent a new (esoteric) scripting language called RoboScript which has a much simpler syntax so non-computer programmers can easily learn how to write scripts in this language which would enable them to properly operate MyRobot. However, you are currently at the initial stage of inventing this new Esolang. The first step to popularize this (esoteric) scripting language is naturally to invent a new editor for it which provides syntax highlighting for this language so your customers feel like they are writing a proper program when they are writing scripts for MyRobot.
+
+Task
+Your MyRobot-specific (esoteric) scripting language called RoboScript only ever contains the following characters: F, L, R, the digits 0-9 and brackets (( and )). Your goal is to write a function highlight which accepts 1 required argument code which is the RoboScript program passed in as a string and returns the script with syntax highlighting. The following commands/characters should have the following colors:
+
+F - Wrap this command around <span style="color: pink"> and </span> tags so that it is highlighted pink in our editor
+L - Wrap this command around <span style="color: red"> and </span> tags so that it is highlighted red in our editor
+R - Wrap this command around <span style="color: green"> and </span> tags so that it is highlighted green in our editor
+Digits from 0 through 9 - Wrap these around <span style="color: orange"> and </span> tags so that they are highlighted orange in our editor
+Round Brackets - Do not apply any syntax highlighting to these characters
+For example:
+
+highlight("F3RF5LF7"); // => "<span style=\"color: pink\">F</span><span style=\"color: orange\">3</span><span style=\"color: green\">R</span><span style=\"color: pink\">F</span><span style=\"color: orange\">5</span><span style=\"color: red\">L</span><span style=\"color: pink\">F</span><span style=\"color: orange\">7</span>"
+And for multiple characters with the same color, simply wrap them with a single <span> tag of the correct color:
+
+highlight("FFFR345F2LL"); // => "<span style=\"color: pink\">FFF</span><span style=\"color: green\">R</span><span style=\"color: orange\">345</span><span style=\"color: pink\">F</span><span style=\"color: orange\">2</span><span style=\"color: red\">LL</span>"
+Note that the use of <span> tags must be exactly the same format as demonstrated above. Even if your solution produces the same visual result as the expected answers, if you miss a space betwen "color:" and "green", for example, you will fail the tests.
+
+Kata in this Series
+RoboScript #1 - Implement Syntax Highlighting
+RoboScript #2 - Implement the RS1 Specification
+RoboScript #3 - Implement the RS2 Specification
+RoboScript #4 - RS3 Patterns to the Rescue
+RoboScript #5 - The Final Obstacle (Implement RSU)
+
+https://www.codewars.com/kata/roboscript-number-1-implement-syntax-highlighting
+*/
+
+namespace Kata\Y2026\Q2;
+
+use PHPUnit\Framework\TestCase;
+
+function highlight(string $code): string {
+	$letters = str_split($code);
+	$finalString = '';
+
+	foreach ($letters as $letterKey => $letter) {
+		if (in_array($letter, ['F', 'L', 'R'], true) || is_numeric($letter)) {
+			if (isset($letters[$letterKey - 1]) && (
+				$letters[$letterKey - 1] === $letter ||
+				(is_numeric($letters[$letterKey - 1]) && is_numeric($letter)))
+			) {
+				$finalString .= $letter;
+			}else {
+				if ($finalString != '' && (isset($letters[$letterKey - 1]) && (in_array($letters[$letterKey - 1], ['F', 'L', 'R'], true)) || is_numeric($letters[$letterKey - 1]))) {
+					$finalString .= '</span>';
+				}
+				$finalString .= sprintf('<span style="color: %s">%s', getColorBasedOnLetter($letter), $letter);
+			}
+		}else {
+			if (isset($letters[$letterKey - 1]) && (in_array($letters[$letterKey - 1], ['F', 'L', 'R'], true) || is_numeric($letters[$letterKey - 1]))) {
+				$finalString .= '</span>';
+			}
+			$finalString .= $letter;
+		}
+	}
+
+	if (in_array($letter, ['F', 'L', 'R'], true) || is_numeric($letter)) {
+		$finalString .= '</span>';
+	}
+	return $finalString;
+}
+
+function getColorBasedOnLetter(string $letter):string
+{
+	return match ($letter) {
+		'F' => 'pink',
+		'L' => 'red',
+		'R' => 'green',
+		default => 'orange',
+	};
+}
+
+class RoboScript1 extends TestCase {
+	public function testDescriptionExamples() {
+		echo "Code without syntax highlighting: F3RF5LF7\r\n";
+		echo "Expected syntax highlighting: <span style=\"color: pink\">F</span><span style=\"color: orange\">3</span><span style=\"color: green\">R</span><span style=\"color: pink\">F</span><span style=\"color: orange\">5</span><span style=\"color: red\">L</span><span style=\"color: pink\">F</span><span style=\"color: orange\">7</span>\r\n";
+		echo "Your code with syntax highlighting: " . highlight("F3RF5LF7") . "\r\n";
+		$this->assertSame("<span style=\"color: pink\">F</span><span style=\"color: orange\">3</span><span style=\"color: green\">R</span><span style=\"color: pink\">F</span><span style=\"color: orange\">5</span><span style=\"color: red\">L</span><span style=\"color: pink\">F</span><span style=\"color: orange\">7</span>", highlight("F3RF5LF7"));
+		echo "Code without syntax highlighting: FFFR345F2LL\r\n";
+		echo "Expected syntax highlighting: <span style=\"color: pink\">FFF</span><span style=\"color: green\">R</span><span style=\"color: orange\">345</span><span style=\"color: pink\">F</span><span style=\"color: orange\">2</span><span style=\"color: red\">LL</span>\r\n";
+		echo "Your code with syntax highlighting: " . highlight("FFFR345F2LL") . "\r\n";
+		$this->assertSame("<span style=\"color: pink\">FFF</span><span style=\"color: green\">R</span><span style=\"color: orange\">345</span><span style=\"color: pink\">F</span><span style=\"color: orange\">2</span><span style=\"color: red\">LL</span>", highlight("FFFR345F2LL"));
+	}
+}
