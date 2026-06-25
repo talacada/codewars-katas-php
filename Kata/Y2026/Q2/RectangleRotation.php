8d7b56afbd620592a2ba348237d9e57057180676
talacada <atalac@proton.me>
2026-06-25 21:44:43 +0200

    RectangleRotation - day 1

---
+++ Kata/Y2026/Q2/RectangleRotation.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+
+Task
+A rectangle with sides equal to even integers a and b is drawn on the Cartesian plane. Its center (the intersection point of its diagonals) coincides with the point (0, 0), but the sides of the rectangle are not parallel to the axes; instead, they are forming 45 degree angles with the axes.
+
+How many points with integer coordinates are located inside the given rectangle (including on its sides)?
+
+Example
+For a = 6 and b = 4, the output should be 23
+
+The following picture illustrates the example, and the 23 points are marked green.
+
+
+
+Input/Output
+[input] integer a
+
+A positive even integer.
+
+Constraints: 2 ≤ a ≤ 10000.
+
+[input] integer b
+
+A positive even integer.
+
+Constraints: 2 ≤ b ≤ 10000.
+
+[output] an integer
+
+The number of inner points with integer coordinates.
+
+
+
+*/
+
+namespace Kata\Y2026\Q2;
+
+use PHPUnit\Framework\TestCase;
+
+function rectangle_rotation($a, $b): int
+{
+
+}
+
+class RectangleRotation extends TestCase
+{
+	public function testBasic()
+	{
+		$this->assertSame(23, rectangle_rotation(6, 4));
+		$this->assertSame(65, rectangle_rotation(30, 2));
+		$this->assertSame(49, rectangle_rotation(8, 6));
+		$this->assertSame(333, rectangle_rotation(16, 20));
+	}
+}
