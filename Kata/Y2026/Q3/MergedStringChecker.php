add7f5c3d501f21831ecc774bb57fb19418032a9
talacada <atalac@proton.me>
2026-07-26 10:20:19 +0200

    MergedStringChecker - day 3 FINISH

--- Kata/Y2026/Q3/MergedStringChecker.php
+++ Kata/Y2026/Q3/MergedStringChecker.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
 
@@ -23,64 +25,56 @@ namespace Kata\Y2026\Q3;
 
 class MergedStringChecker
 {
+    public array $full;
+    public array $part1;
+    public array $part2;
+    public function __construct(string $full, string $part1, string $part2)
+    {
+        $this->full = $full === '' ? [] : str_split($full);
+        $this->part1 = $part1 === '' ? [] : str_split($part1);
+        $this->part2 = $part2 === '' ? [] : str_split($part2);
+    }
+    public function check(array $full, array $part1, array $part2): bool
+    {
+        $oneIndex = 0;
+        $twoIndex = 0;
+        if (count($part1) + count($part2) != count($full)) {
+            return false;
+        }
 
-	public array $full;
-	public array $part1;
-	public array $part2;
-	public function __construct(string $full, string $part1, string $part2)
-	{
-		$this->full = str_split($full);
-		if ($part1 != '') {
-			$this->part1 = str_split($part1);
-		}else{
-			$this->part1 = [];
-		}
-		if ($part2 != '') {
-			$this->part2 = str_split($part2);
-		}else{
-			$this->part2 = [];
-		}
-	}
-	public function check(array $full, array $part1, array $part2): bool
-	{
-		$oneIndex = 0;
-		$twoIndex = 0;
-		if (count($part1) + count($part2) != count($full)) {
-			return false;
-		}
+        if ($part1 === []) {
+            return implode($full) === implode($part2);
+        }
+        if ($part2 === []) {
+            return implode($full) === implode($part1);
+        }
+        foreach ($full as $index => $fullLetter) {
+            if (isset($part1[$oneIndex]) && isset($part2[$twoIndex])) {
+                if ($part1[$oneIndex] === $part2[$twoIndex]) {
+                    $subfull = str_split(substr(implode("", $full), $index + 1));
+                    $subpart1Smaller = str_split(substr(implode("", $part1), $oneIndex + 1));
+                    $subpart2Smaller = str_split(substr(implode("", $part2), $twoIndex + 1));
+                    $subpart1Normal = str_split(substr(implode("", $part1), $oneIndex));
+                    $subpart2Normal = str_split(substr(implode("", $part2), $twoIndex));
+                    $resultOne = $this->check($subfull, $subpart1Smaller, $subpart2Normal);
+                    $resultTwo = $this->check($subfull, $subpart1Normal, $subpart2Smaller);
+                    if ($resultOne || $resultTwo) {
+                        return true;
+                    }
+					return false;
+                }
+            }
+            if (isset($part1[$oneIndex]) && $fullLetter === $part1[$oneIndex]) {
+                $oneIndex++;
+                continue;
+            }
+            if (isset($part2[$twoIndex]) && $fullLetter === $part2[$twoIndex]) {
+                $twoIndex++;
+                continue;
+            }
+            return false;
+        }
 
-		if ($part1 === []) {
-			return implode($full) === implode($part2);
-		}
-		if ($part2 === []) {
-			return implode($full) === implode($part1);
-		}
-		foreach ($full as $index =>$fullLetter) {
-			if (isset($part1[$oneIndex]) && isset($part2[$twoIndex])) {
-				if ($part1[$oneIndex] === $part2[$twoIndex]) {
-					$subfull = str_split(substr(implode("", $full), $index + 1));
-					$subpart1Smaller = str_split(substr(implode("", $part1), $oneIndex + 1));
-					$subpart2Smaller = str_split(substr(implode("", $part2), $twoIndex + 1));
-					$subpart1Normal = str_split(substr(implode("", $part1), $oneIndex));
-					$subpart2Normal = str_split(substr(implode("", $part2), $twoIndex));
-					$resultOne = $this->check($subfull, $subpart1Smaller, $subpart2Normal);
-					$resultTwo = $this->check($subfull, $subpart1Normal, $subpart2Smaller);
-					if ($resultOne || $resultTwo) {
-						return true;
-					}
-				}
-			}
-			if (isset($part1[$oneIndex]) && $fullLetter === $part1[$oneIndex]) {
-				$oneIndex++;
-				continue;
-			}
-			if (isset($part2[$twoIndex]) && $fullLetter === $part2[$twoIndex]) {
-				$twoIndex++;
-				continue;
-			}
-			return false;
-		}
-
-		return true;
-	}
+        return true;
+    }
 }
--- Tests/Y2026/Q3/MergedStringCheckerTest.php
+++ Tests/Y2026/Q3/MergedStringCheckerTest.php
@@ -9,19 +9,22 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../../../Kata/Y2026/Q3/MergedStringChecker.php';
 
-function isMerge($s, $part1, $part2) {
-	$checker = new MergedStringChecker($s, $part1, $part2);
-	return $checker->check($checker->full, $checker->part1, $checker->part2);
+function isMerge(string $s, string $part1, string $part2): bool
+{
+    $checker = new MergedStringChecker($s, $part1, $part2);
+    return $checker->check($checker->full, $checker->part1, $checker->part2);
 }
 
 class MergedStringCheckerTest extends TestCase
 {
-	public function testSampleTestCase(): void {
-		$this->assertSame(true, isMerge('Bananas from Bahamas', 'Bahas', 'Bananas from am'));
-		$this->assertSame(true, isMerge('codewars', 'codewars', ''));
-		$this->assertSame(false, isMerge('codewars', 'code', 'warss'));
-		$this->assertSame(true, isMerge('codewars', 'code', 'wars'));
-		$this->assertSame(true, isMerge('codewars', 'cdw', 'oears'));
-		$this->assertSame(false, isMerge('codewars', 'cod', 'wars'));
-	}
+    public function testSampleTestCase(): void
+    {
+        $this->assertSame(true, isMerge('', '', ''));
+        $this->assertSame(true, isMerge('Bananas from Bahamas', 'Bahas', 'Bananas from am'));
+        $this->assertSame(true, isMerge('codewars', 'codewars', ''));
+        $this->assertSame(false, isMerge('codewars', 'code', 'warss'));
+        $this->assertSame(true, isMerge('codewars', 'code', 'wars'));
+        $this->assertSame(true, isMerge('codewars', 'cdw', 'oears'));
+        $this->assertSame(false, isMerge('codewars', 'cod', 'wars'));
+    }
 }
