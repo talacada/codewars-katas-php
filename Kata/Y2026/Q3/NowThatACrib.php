3788b7f6aa2615d2174c32a22bae0d59a4dd482a
talacada <atalac@proton.me>
2026-07-29 20:06:59 +0200

    NowThatACrib - day 1 start

---
+++ Kata/Y2026/Q3/NowThatACrib.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+
+If you haven't solved it already I recommend trying this kata first.
+
+Task
+Given n representing the number of floors build a penthouse like this:
+
+        ___
+       /___\
+      /_____\
+      |  _  |     1 floor
+      |_|_|_|
+
+       _____
+      /_____\
+     /_______\
+    /_________\
+   /___________\
+   |           |
+   |    ___    |     2 floors
+   |   |   |   |
+   |___|___|___|
+
+      _______
+     /_______\
+    /_________\
+   /___________\
+  /_____________\
+ /_______________\
+/_________________\
+|                 |         3 floors
+|                 |
+|      _____      |
+|     |     |     |
+|     |     |     |
+|_____|_____|_____|
+Note: whitespace should be preserved on both sides of the roof. No invalid input tests.
+
+Good luck!
+
+https://www.codewars.com/kata/58360d112fb0ba255300008b
+*/
+
+namespace Y2026\Q3;
+
+function my_crib(string $n): string {
+	$crib = '';
+	$roofOuterMargin = $n;
+	$roofInnerMargin = 0;
+	//Roof
+	for ($roofFloor = $n; $roofFloor >= 0; $roofFloor--) {
+		$crib .= str_repeat(' ', $roofOuterMargin);
+		$crib .= '/';
+
+		$crib .= str_repeat('_', $roofInnerMargin * 2);
+
+		$crib .= '\\';
+		$crib .= str_repeat(' ', $roofOuterMargin);
+
+		$crib .= '\n';
+
+		$roofInnerMargin++;
+		$roofOuterMargin--;
+	}
+	//Body
+	for ($floor = $n; $floor > 0; $floor--) {
+		$crib .= '|';
+		if ($floor === 1) {
+			$crib .= str_repeat('_', $n * 2);
+		} else {
+			$crib .= str_repeat(' ', $n * 2);
+		}
+		$crib .= '|\n';
+	}
+
+	return rtrim($crib, "\\n");
+}
