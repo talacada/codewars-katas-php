eb8be157056bf4c81d17b55a8de86594541a22c4
talacada <atalac@proton.me>
2026-08-12 13:32:59 +0200

    Snail - day 3 FINISH

--- Kata/Y2026/Q3/Snail.php
+++ Kata/Y2026/Q3/Snail.php
@@ -30,57 +30,81 @@ namespace Kata\Y2026\Q3;
 
 class Snail
 {
-	private array $matrix;
-	private int $topOffset = 0;
-	private int $bottomOffset = 0;
-	private int $leftOffset = 0;
-	private int $rightOffset = 0;
-	private int $nowSide = 0;
-	private int $direction = 0;
+    private array $matrix;
+    private int $topOffset = 0;
+    private int $bottomOffset = 0;
+    private int $leftOffset = 0;
+    private int $rightOffset = 0;
+    private int $nowSide = 0;
+    public function __construct(array $array)
+    {
+        $this->matrix = $array;
+    }
+    public function solve(): array
+    {
+        $return = [];
 
-	public function __construct(array $array)
-	{
-		$this->matrix = $array;
-	}
-	public function solve():array
-	{
-		$return = [];
+        do {
+            if ($this->nowSide === 0 || $this->nowSide === 2) {
+                array_push($return, ...$this->getRow());
+            } else {
+                array_push($return, ...$this->getCol());
+            }
 
-		do {
-			if ($this->nowSide === 0) {
-				$return[] = $this->getRow();
-			} else {
-				$return[] = $this->getCol();
-			}
-		} while (count(...$this->matrix) != count($return));
+            $this->updateNowSide();
+        } while (count($this->matrix) * count($this->matrix[0]) != count($return));
 
-		return $return;
-	}
+        return $return;
+    }
 
-	private function getRow(): array
-	{
-		if ($this->direction === 0) {
-			$return = array_merge($this->matrix[$this->topOffset]);
-			$this->topOffset++;
-		}else {
-			$return = $this->matrix[$this->bottomOffset];
-			$this->bottomOffset++;
-		}
+    private function getRow(): array
+    {
+        if ($this->nowSide === 0) {
+            $return = $this->matrix[$this->topOffset];
+            $this->topOffset++;
+        } else {
+            $return = $this->matrix[(int) array_key_last($this->matrix) - $this->bottomOffset];
+            $this->bottomOffset++;
+        }
 
-		$return = array_slice($return, $this->leftOffset);
-		if ($this->rightOffset > 0) {
-			$return = array_slice($return, 0, -$this->rightOffset);
-		}
+        $return = array_slice($return, $this->leftOffset);
+        if ($this->rightOffset > 0) {
+            $return = array_slice($return, 0, -$this->rightOffset);
+        }
 
-			if ($this->direction === 1) {
-			$return = array_reverse($return);
-		}
+        if ($this->nowSide === 2) {
+            $return = array_reverse($return);
+        }
 
-		return $return;
-	}
+        return $return;
+    }
 
-	private function getCol(): array
-	{
+    private function getCol(): array
+    {
+        $return = [];
+        if ($this->nowSide === 1) {
+            for ($row = $this->topOffset; $row < count($this->matrix) - $this->bottomOffset; $row++) {
+                $return[] = $this->matrix[$row][(int) array_key_last($this->matrix) - $this->rightOffset];
+            }
+            $this->rightOffset++;
+        } else {
+            for ($row = $this->topOffset; $row < count($this->matrix) - $this->bottomOffset; $row++) {
+                $return[] = $this->matrix[$row][$this->leftOffset];
+            }
+            $this->leftOffset++;
+            $return = array_reverse($return);
+        }
 
-	}
+        return $return;
+
+    }
+
+    private function updateNowSide(): void
+    {
+        if ($this->nowSide !== 3) {
+            $this->nowSide++;
+        } else {
+            $this->nowSide = 0;
+        }
+    }
 }
