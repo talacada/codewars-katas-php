0d6e1693fabfb21f5f68dafed140a272ed6fc65a
talacada <atalac@proton.me>
2026-08-18 08:48:46 +0200

    SumStringsAsNumbers - day 3 now fully working, need refactor

--- Kata/Y2026/Q3/SumStringsAsNumbers.php
+++ Kata/Y2026/Q3/SumStringsAsNumbers.php
@@ -17,39 +17,43 @@ https://www.codewars.com/kata/5324945e2ece5e1f32000370
 
 namespace Kata\Y2026\Q3\SumStringsAsNumbers;
 
-/*
-
-count it like two numbers under each other like in school math, cause int has 15 characters than overflow....
-
-*/
 function sum_strings($a, $b): string
 {
-	$bonus = 0;
-	$aRev = strrev($a);
-	$bRev = strrev($b);
-	$final = '';
-	for ($i = 0; $i < max(strlen($a), strlen($b)); $i++) {
-		$aNow = (int) substr($aRev, $i, 1);
-		$bNow = (int) substr($bRev, $i, 1);
-		if ($aNow +  $bNow + $bonus > 9) {
-			$final .= $aNow + $bNow + $bonus - 10;
-			if ($bonus != 0) {
-				$bonus -= 1;
-			}
-			$bonus += 1;
-		}else {
-			$final .= $aNow + $bNow + $bonus;
-			if ($bonus != 0) {
-				$bonus -= 1;
-			}
-
-		}
-	}
-
-	return strrev($final);
+	$calculator = new SumStringsAsNumbers();
+	return $calculator->result($a, $b);
 }
 
 class SumStringsAsNumbers
 {
+	public function result($a, $b): string
+	{
+		$bonus = 0;
+		$aRev = strrev($a);
+		$bRev = strrev($b);
+		$final = '';
+		$maxLength = max(strlen($a), strlen($b));
+		for ($i = 0; $i < $maxLength; $i++) {
+			$aNow = (int) substr($aRev, $i, 1);
+			$bNow = (int) substr($bRev, $i, 1);
+			if ($aNow +  $bNow + $bonus > 9) {
+				if ($i === $maxLength - 1) {
+					$final .= strrev($aNow + $bNow + $bonus);
+				}else {
+					$final .= $aNow + $bNow + $bonus - 10;
+				}
+				if ($bonus != 0) {
+					$bonus -= 1;
+				}
+				$bonus += 1;
+			}else {
+				$final .= $aNow + $bNow + $bonus;
+				if ($bonus != 0) {
+					$bonus -= 1;
+				}
 
+			}
+		}
+
+		return ltrim(strrev($final), "0");
+	}
 }
