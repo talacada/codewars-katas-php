6bac36c9382dc82923633d3398de4a93a912bd84
talacada <atalac@proton.me>
2026-08-29 12:18:13 +0200

    TwiceLinear - day 1 start

---
+++ Tests/Y2026/Q3/TwiceLinearTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Y2026\Q3\TwiceLinear;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../../Kata/Y2026/Q3/TwiceLinear.php.php';
+use function Kata\Y2026\Q3\TwiceLinear\dblLinear;
+
+class TwiceLinearTest extends TestCase
+{
+	private function revTest($actual, $expected): void {
+		$this->assertSame($expected, $actual);
+	}
+	public function testBasics(): void {
+		$this->revTest(dblLinear(10), 22);
+		$this->revTest(dblLinear(20), 57);
+		$this->revTest(dblLinear(30), 91);
+		$this->revTest(dblLinear(50), 175);
+		$this->revTest(dblLinear(100), 447);
+	}
+}
