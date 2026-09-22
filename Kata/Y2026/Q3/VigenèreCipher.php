0e97e7e6da478039ccb971684e4406e77c50b303
Talač Adam <98701868+talacada@users.noreply.github.com>
2026-09-22 15:29:28 +0200

    Kata/vigenere ciphre (#29)
    * MatrixDeterminant - day 1 start
    
    * MatrixDeterminant - day 2 encoding
    
    * MatrixDeterminant - day 3 encode working
    
    * MatrixDeterminant - day 4 local tests working
    
    * MatrixDeterminant - day 5 analyzing problems
    
    * MatrixDeterminant - day 6 unified function for encode/decode with switch
    
    * MatrixDeterminant - day 7 FINISH

---
+++ Kata/Y2026/Q3/VigenèreCipher.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+
+In this kata, you will implement cipher functions using utf-8 strings.
+
+The Vigenère cipher is a classic cipher originally developed by Italian cryptographer Giovan Battista Bellaso and published in 1553. It is named after a later French cryptographer Blaise de Vigenère, who had developed a stronger autokey cipher (a cipher that incorporates the message of the text into the key). The cipher is easy to understand and implement, but survived three centuries of attempts to break it, earning it the nickname "le chiffre indéchiffrable" ("the unbreakable cipher")
+
+The Vigenère cipher is a method of encrypting alphabetic text by using a series of different Caesar ciphers based on the letters of a keyword. It is a simple form of polyalphabetic substitution.
+
+In a Caesar cipher, each letter of the alphabet is shifted along some number of places; for example, in a Caesar cipher of shift 3, A would become D, B would become E, Y would become B and so on. The Vigenère cipher consists of several Caesar ciphers in sequence with different shift values.
+
+Assume the key is repeated for the length of the text, character by character. The key advances for every character in the message, regardless of whether that character belongs to the alphabet. Characters not in the alphabet are not encrypted, but they still consume one character of the key. This is different than some other implementations of the Vigenère Cipher.
+
+The shift is derived by applying a Caesar shift to a character with the corresponding index of the key in the alphabet.
+
+Visual representation:
+
+"abcdefghijklmnopqrstuvwxyz"       // alphabet
+"my secret code i want to secure"  // message
+"passwordpasswordpasswordpasswor"  // key
+
+// The space between "my" and "secret" consumes the
+// first "s" of the key, even though the space character
+// is not in the alphabet and will not be encrypted.
+Write a class that, when given a key and an alphabet, can be used to encode and decode from the cipher.
+
+Examples
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+key      = "password"
+
+"codewars" --> encode -->  "rovwsoiv"
+"laxxhsj"  --> decode -->  "waffles"
+Note: any character not in the alphabet must be left alone. For example in the above case:
+
+"CODEWARS"  --> encode -->  "CODEWARS"
+
+https://www.codewars.com/kata/52d1bd3694d26f8d6e0000d3
+
+*/
+
+namespace Kata\Y2026\Q3;
+
+class VigenèreCipher
+{
+    private string $alphabet;
+    private array $key;
+
+    public function __construct(string $key, string $alphabet)
+    {
+        foreach (str_split($key) as $letter) {
+            if (str_contains($alphabet, $letter)) {
+                $this->key[] = strpos($alphabet, $letter);
+            } else {
+                $this->key[] = null;
+            }
+        }
+
+        $this->alphabet = $alphabet;
+    }
+
+    public function encode(string $message): string
+    {
+        return $this->transform($message, 'encode');
+    }
+
+    public function decode(string $message): string
+    {
+        return $this->transform($message, 'decode');
+    }
+
+    private function transform(string $message, string $mode): string
+    {
+        $transformed = '';
+        $keyIndex = 0;
+        $alphabetLength = strlen($this->alphabet);
+
+        foreach (str_split($message) as $letter) {
+            if (str_contains($this->alphabet, $letter)) {
+                $cesarShift = (int) strpos($this->alphabet, $letter);
+                $keyShift = (int) $this->key[$keyIndex];
+                $newIndex = $this->getShiftByMode($mode, $keyShift, $cesarShift, $alphabetLength);
+                $transformed .= substr($this->alphabet, $newIndex, 1);
+            } else {
+                $transformed .= $letter;
+            }
+
+            if ($keyIndex < count($this->key) - 1) {
+                $keyIndex++;
+            } else {
+                $keyIndex = 0;
+            }
+        }
+
+        return $transformed;
+    }
+
+    private function getShiftByMode(string $mode, int $keyShift, int $cesarShift, int $alphabetLength): int
+    {
+        $newShift = 0;
+        switch ($mode) {
+            case 'encode':
+                if ($cesarShift + $keyShift >= $alphabetLength) {
+                    $newShift = $cesarShift + $keyShift - $alphabetLength;
+                } else {
+                    $newShift = $keyShift + $cesarShift;
+                }
+                break;
+            case 'decode':
+                if ($cesarShift - $keyShift < 0) {
+                    $newShift = $cesarShift - $keyShift + $alphabetLength;
+                } else {
+                    $newShift = $cesarShift - $keyShift;
+                }
+        }
+
+        return $newShift;
+    }
+}
---
+++ Tests/Y2026/Q3/VigenereCipherTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Y2026\Q3;
+
+use Kata\Y2026\Q3\VigenèreCipher;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../../Kata/Y2026/Q3/VigenèreCipher.php';
+
+
+
+class VigenereCipherTest extends TestCase
+{
+    public function test1(): void
+    {
+        $abc = 'abcdefghijklmnopqrstuvwxyz';
+        $key = 'password';
+        $c = new VigenèreCipher($key, $abc);
+
+        $this->assertSame('rovwsoiv', $c->encode('codewars'));
+        $this->assertSame('codewars', $c->decode('rovwsoiv'));
+        $this->assertSame('laxxhsj', $c->encode('waffles'));
+        $this->assertSame('waffles', $c->decode('laxxhsj'));
+        $this->assertSame('CODEWARS', $c->encode('CODEWARS'));
+        $this->assertSame('CODEWARS', $c->decode('CODEWARS'));
+        $this->assertSame("xt'k o vwixl qzswej!", $c->encode("it's a shift cipher!"));
+        $this->assertSame("it's a shift cipher!", $c->decode("xt'k o vwixl qzswej!"));
+    }
+
+    public function test2(): void
+    {
+        $abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $key = 'PASSWORD';
+        $c = new VigenèreCipher($key, $abc);
+
+        $this->assertSame('CODEWARS', $c->decode('ROVWSOIV'));
+        $this->assertSame('ROVWSOIV', $c->encode('CODEWARS'));
+        $this->assertSame('LAXXHSJ', $c->encode('WAFFLES'));
+        $this->assertSame('WAFFLES', $c->decode('LAXXHSJ'));
+        $this->assertSame('codewars', $c->encode('codewars'));
+        $this->assertSame('codewars', $c->decode('codewars'));
+    }
+
+    public function test3(): void
+    {
+        $abc = 'abcdefghijklmnopqrstuvwxyz';
+        $key = 'pizza';
+        $c = new VigenèreCipher($key, $abc);
+
+        $this->assertSame('rwcdwpzr', $c->encode('codewars'));
+        $this->assertSame('codewars', $c->decode('rwcdwpzr'));
+        $this->assertSame('lieelta', $c->encode('waffles'));
+        $this->assertSame('waffles', $c->decode('lieelta'));
+        $this->assertSame('pa_hf', $c->encode('as_if'));
+        $this->assertSame('as_if', $c->decode('pa_hf'));
+        $this->assertSame('CODEWARS', $c->encode('CODEWARS'));
+        $this->assertSame('CODEWARS', $c->decode('CODEWARS'));
+    }
+
+    public function test4(): void
+    {
+        $abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $key = 'PIZZA';
+        $c = new VigenèreCipher($key, $abc);
+
+        $this->assertSame('CODEWARS', $c->decode('RWCDWPZR'));
+        $this->assertSame('RWCDWPZR', $c->encode('CODEWARS'));
+        $this->assertSame('LIEELTA', $c->encode('WAFFLES'));
+        $this->assertSame('WAFFLES', $c->decode('LIEELTA'));
+        $this->assertSame('PA_HF', $c->encode('AS_IF'));
+        $this->assertSame('AS_IF', $c->decode('PA_HF'));
+        $this->assertSame('codewars', $c->encode('codewars'));
+        $this->assertSame('codewars', $c->decode('codewars'));
+    }
+}
